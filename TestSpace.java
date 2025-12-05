12173663d041d2fa7e5f967c27f26d5213cf41cd
Mishidev-2 <shimizumarkrey2007@gmail.com>
2025-12-05 13:40:24 +0800

    Js some edits - testing a few things

--- TestSpace.java
+++ TestSpace.java
@@ -4,6 +4,21 @@ public class TestSpace{
    public static void main(String[] args){
       Scanner scanner = new Scanner(System.in);
          
+
+      GameTools.typeText("The path ahead does not so much end as it is severed.\n"); GameTools.delay(0.7);
+      GameTools.typeText("One moment you are walking on the grey, "); GameTools.delay(0.3);
+      GameTools.typeText("dust-fine soil of the silent plains, "); GameTools.delay(0.3);
+      GameTools.typeText("and the next you stand before a sheer cliff of impossibly smooth, "); GameTools.delay(0.3);
+      GameTools.typeText("black glass.\n"); GameTools.delay(0.5);
+      GameTools.typeText("Set into it is a doorway that should not exist.\n"); GameTools.delay(0.7);
+
+      GameTools.typeText("Sealed by a doorway of chains frozen in ice. "); GameTools.delay(0.5);
+      GameTools.typeText("A sterile light glows within, "); GameTools.delay(0.3);
+      GameTools.typeText("and all sound dies as you approach.\n"); GameTools.delay(0.5);
+      GameTools.typeText("This is not a passage, "); GameTools.delay(0.3);
+      GameTools.typeText("but a seal - "); GameTools.delay(0.3);
+      GameTools.typeText("a silence never meant to be broken.\n"); GameTools.delay(0.5);
+
       GameTools.typeText("\n<Gatekeeper of Gluttony:>\n");
       GameTools.delay(0.7);
       GameTools.typeText("\"You reek of decay, "); GameTools.delay(0.3);
